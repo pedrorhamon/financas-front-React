0c87103b1da6a43680c26313396546039af8eac2
pedrorhamon <pedrorhamon16@gmail.com>
2021-11-21 18:00:00 -0300

    iniciando tela de login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+
+import Login from './views/login'
+
 import 'bootswatch/dist/flatly/bootstrap.css'
+
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <button type="button" className="btn btn-primary" > Primary</button>
+        <Login/>
       </div>
     )
   }
---
+++ src/views/login.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Login extends React.Component{
+
+    render(){
+        return(
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6" style={{position: 'relative', left:'300px'}}>
+
+                </div>
+            </div>
+        </div>
+        )
+    }
+}
+
+export default Login;
