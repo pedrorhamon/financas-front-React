ddca28b73bfe0f783305efe7ac6b9bb833cbf090
pedrorhamon <pedrorhamon16@gmail.com>
2021-11-21 19:21:05 -0300

    Criando card

--- src/views/login.js
+++ src/views/login.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Card from '../components/card'
+
 class Login extends React.Component{
 
     render(){
@@ -7,7 +9,11 @@ class Login extends React.Component{
         <div className="container">
             <div className="row">
                 <div className="col-md-6" style={{position: 'relative', left:'300px'}}>
-
+                    <div className="bs-docs-section">
+                        <Card title = "Login">
+                            Meu cartão
+                        </Card>
+                    </div>
                 </div>
             </div>
         </div>
