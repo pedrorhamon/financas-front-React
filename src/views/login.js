fdf86cba41df16d3bc92801509996a341c8692b7
pedrorhamon <pedrorhamon16@gmail.com>
2021-11-22 20:00:05 -0300

    Componentizando o formulário de Login

--- src/views/login.js
+++ src/views/login.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Card from '../components/card'
+import FormGroup from "../components/form-group";
 
 class Login extends React.Component{
 
@@ -11,7 +12,27 @@ class Login extends React.Component{
                 <div className="col-md-6" style={{position: 'relative', left:'300px'}}>
                     <div className="bs-docs-section">
                         <Card title = "Login">
-                            Meu cartão
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <div className="bs-component">
+                                        <fieldset>
+                                           <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
+                                               <input type="email" 
+                                               className="form-control" 
+                                               id="exampleInputEmail1"
+                                               aria-describedby="emailHelp" 
+                                               placeholder="Digite um email"/>
+                                           </FormGroup>
+                                           <FormGroup label = "Senha: *" htmlFor="exampleInputPassword1">
+                                           <input type="password" 
+                                           class="form-control"
+                                            id="exampleInputPassword1" 
+                                            placeholder="Password"/>
+                                           </FormGroup>
+                                        </fieldset>
+                                    </div>
+                                </div>
+                            </div>
                         </Card>
                     </div>
                 </div>
