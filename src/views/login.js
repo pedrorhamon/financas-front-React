8d1b04a6ad747e8650b9f52b14716dc83d497b91
pedrorhamon <pedrorhamon16@gmail.com>
2021-11-30 22:42:34 -0300

    integração com o backend

--- src/views/login.js
+++ src/views/login.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { withRouter } from "react-router-dom";
+import axios from "axios";
 
 import Card from '../components/card'
 import FormGroup from "../components/form-group";
@@ -13,8 +14,14 @@ class Login extends React.Component {
     }
 
     entrar = () => {
-        console.log('Email', this.state.email);
-        console.log('Senha', this.state.senha);
+        axios.post('http://localhost:8080/api/usuarios/autenticar', {
+            email: this.state.email,
+            senha: this.state.senha
+        }).then(response => {
+            console.log(response);
+        }).catch(erro => {
+            console.log(erro.response);
+        })
     }
 
     prepareCadastrar = () => {
