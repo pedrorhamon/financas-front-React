eda9dba5e42f374783af1004dfa0719531136f55
pedrorhamon <pedrorhamon16@gmail.com>
2021-12-06 22:49:54 -0300

    Mostrando mensagens ao usuários

--- src/main/App.js
+++ src/main/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 import Rotas from './rotas';
 import NavBar from '../components/navbar';
+import 'toastr/build/toastr.min.js'
 
 import 'bootswatch/dist/flatly/bootstrap.css'
 import '../custom.css'
+import 'toastr/build/toastr.css'
 
 
 class App extends React.Component {
--- src/views/login.js
+++ src/views/login.js
@@ -7,13 +7,13 @@ import LocalStorageService from '../app/service/localstorageService'
 
 import Card from '../components/card'
 import FormGroup from "../components/form-group";
+import {mensagemErro} from '../components/toastr';
 
 class Login extends React.Component {
 
     state = {
         email: '',
-        senha: '',
-        mensagemErro: null
+        senha: ''
     }
 
     constructor() {
@@ -30,7 +30,7 @@ class Login extends React.Component {
             LocalStorageService.adicionarItem('_usuario_logado', response.data)
             this.props.history.push('/home')
         }).catch(erro => {
-            this.setState({ mensagemErro: erro.response.data })
+            mensagemErro(erro.response.data)
         })
     }
 
@@ -44,9 +44,6 @@ class Login extends React.Component {
                 <div className="col-md-6" style={{ position: 'relative', left: '300px' }}>
                     <div className="bs-docs-section">
                         <Card title="Login">
-                            <div className="row">
-                                <span color="danger">{this.state.mensagemErro}</span>
-                            </div>
                             <div className="row">
                                 <div className="col-lg-12">
                                     <div className="bs-component">
