20cb03cbe390d10bf8be5611ef312b6c5b72713e
pedrorhamon <pedrorhamon16@gmail.com>
2021-12-17 19:37:40 -0300

    Melhorias na view de consulta Lancamentos

--- src/views/lancamentos/consulta-lancamentos.js
+++ src/views/lancamentos/consulta-lancamentos.js
@@ -7,6 +7,7 @@ import LancamentosTable from "./lancamentosTable";
 
 import LancamentoService from "../../app/service/lancamentoService";
 import LocalStorageService from '../../app/service/localstorageService';
+import * as mensagens from "../../components/toastr";
 
 class ConsultaLancamentos extends React.Component {
 
@@ -24,6 +25,11 @@ class ConsultaLancamentos extends React.Component {
     }
 
     buscar = () => {
+        if (!this.state.ano){
+            mensagens.mensagemErro('O preenchimento do campo obrigatório.')
+            return false;
+        }
+
         const usuarioLogado = LocalStorageService.obterItem('_usuario_logado');
 
         const lancamentoFiltro = {
@@ -71,13 +77,20 @@ class ConsultaLancamentos extends React.Component {
                                 <p></p>
                             </FormGroup>
                                 <p></p>
-                            <FormGroup htmlFor="inputTipo" label="Tipo Lançamento: ">
-                                <SelectMenu id="inputTipo" value={this.state.tipo}
-                                    onChange={e => this.setState({ tipo: e.target.value })} className='form-control' lista={tipos} />
+                                <FormGroup htmlFor="inputTipo" label="Tipo Lançamento: ">
+                                <SelectMenu id="inputTipo" 
+                                            value={this.state.tipo}
+                                            onChange={e => this.setState({ tipo: e.target.value })}
+                                            className="form-control" 
+                                            lista={tipos} />
                             </FormGroup>
                             <p></p>
 
-                            <button type="button" onClick={this.buscar()} className="btn btn-success">Buscar</button>
+                            <button onClick={this.buscar}
+                                type="button"
+                                className="btn btn-success">
+                                <i className="pi pi-search"></i> Buscar
+                            </button>
                             <button type="button" className="btn btn-danger">Cadastrar</button>
                         </div>
                     </div>
