9f1464f5f3d1b75c577626b3924688db367c7057
pedrorhamon <pedrorhamon16@gmail.com>
2021-12-21 21:38:14 -0300

    Adicionando e testando a biblioteca do Primereact no projeto

--- src/main/App.js
+++ src/main/App.js
@@ -4,9 +4,14 @@ import Rotas from './rotas';
 import NavBar from '../components/navbar';
 import 'toastr/build/toastr.min.js'
 
+
 import 'bootswatch/dist/flatly/bootstrap.css'
 import '../custom.css'
 import 'toastr/build/toastr.css'
+
+import 'primereact/resources/themes/lara-light-indigo/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
 
 
 class App extends React.Component {
